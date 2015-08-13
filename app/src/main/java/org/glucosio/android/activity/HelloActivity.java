6c88f62d77c657e2211b3916acb687521dc5bbc3
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-08-13 21:16:01 +0200

    Added HomePageAdapter and Fragments. Implemented TabLayout.

--- app/src/main/java/org/glucosio/android/activity/HelloActivity.java
+++ app/src/main/java/org/glucosio/android/activity/HelloActivity.java
@@ -3,13 +3,13 @@ package org.glucosio.android.activity;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.glucosio.android.R;
 import org.glucosio.android.db.DatabaseHandler;
---
+++ app/src/main/java/org/glucosio/android/fragment/OverviewFragment.java
@@ -0,0 +1,37 @@
+package org.glucosio.android.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.glucosio.android.R;
+
+public class OverviewFragment extends Fragment {
+
+    public static HistoryFragment newInstance() {
+        HistoryFragment fragment = new HistoryFragment();
+
+
+        return fragment;
+    }
+
+    public OverviewFragment() {
+        // Required empty public constructor
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+}
