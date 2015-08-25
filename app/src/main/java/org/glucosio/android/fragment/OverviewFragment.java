d43018c65eaed98d7272e5de8f656037341d7614
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-08-25 21:37:45 +0200

    Improved graph.

--- app/src/main/java/org/glucosio/android/fragment/OverviewFragment.java
+++ app/src/main/java/org/glucosio/android/fragment/OverviewFragment.java
@@ -23,8 +23,14 @@ import org.glucosio.android.R;
 import org.glucosio.android.activity.MainActivity;
 import org.glucosio.android.db.DatabaseHandler;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class OverviewFragment extends Fragment {
 
@@ -68,28 +74,52 @@ public class OverviewFragment extends Fragment {
         XAxis xAxis = chart.getXAxis();
         xAxis.setDrawGridLines(false);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(getResources().getColor(R.color.glucosio_text_light));
 
-        LimitLine ll1 = new LimitLine(130f, "");
-        ll1.setLineWidth(2f);
-        ll1.setLineColor(getResources().getColor(R.color.glucosio_accent));
+        LimitLine ll1 = new LimitLine(130f, "High");
+        ll1.setLineWidth(1f);
+        ll1.setLineColor(getResources().getColor(R.color.glucosio_gray_light));
+        ll1.setTextColor(getResources().getColor(R.color.glucosio_text));
 
-        LimitLine ll2 = new LimitLine(70f, "");
-        ll2.setLineWidth(2f);
-        ll2.setLineColor(getResources().getColor(R.color.glucosio_accent));
+
+        LimitLine ll2 = new LimitLine(70f, "Low");
+        ll2.setLineWidth(1f);
+        ll2.setLineColor(getResources().getColor(R.color.glucosio_gray_light));
+        ll2.setTextColor(getResources().getColor(R.color.glucosio_text));
+
+        LimitLine ll3 = new LimitLine(200f, "Hyper");
+        ll3.setLineWidth(1f);
+        ll3.enableDashedLine(10, 10, 10);
+        ll3.setLineColor(getResources().getColor(R.color.glucosio_gray_light));
+        ll3.setTextColor(getResources().getColor(R.color.glucosio_text));
+
+
+        LimitLine ll4 = new LimitLine(50f, "Hypo");
+        ll4.setLineWidth(1f);
+        ll4.enableDashedLine(10, 10, 10);
+        ll4.setLineColor(getResources().getColor(R.color.glucosio_gray_light));
+        ll4.setTextColor(getResources().getColor(R.color.glucosio_text));
+
 
         YAxis leftAxis = chart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
         leftAxis.addLimitLine(ll1);
         leftAxis.addLimitLine(ll2);
+        leftAxis.addLimitLine(ll3);
+        leftAxis.addLimitLine(ll4);
+        leftAxis.setTextColor(getResources().getColor(R.color.glucosio_text_light));
         leftAxis.setStartAtZero(false);
         //leftAxis.setYOffset(20f);
         leftAxis.disableGridDashedLine();
+        leftAxis.setDrawGridLines(false);
 
         // limit lines are drawn behind data (and not on top)
         leftAxis.setDrawLimitLinesBehindData(true);
 
         chart.getAxisRight().setEnabled(false);
-
+        chart.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        chart.setDescription("");
+        chart.setGridBackgroundColor(Color.parseColor("#FFFFFF"));
         setData();
         legend.setEnabled(false);
         chart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
@@ -100,8 +130,10 @@ public class OverviewFragment extends Fragment {
     private void setData() {
 
         ArrayList<String> xVals = new ArrayList<String>();
+
         for (int i = 0; i < datetime.size(); i++) {
-            xVals.add(datetime.get(i) + "");
+            String date = convertDate(datetime.get(i));
+            xVals.add(date + "");
         }
 
         ArrayList<Entry> yVals = new ArrayList<Entry>();
@@ -121,8 +153,9 @@ public class OverviewFragment extends Fragment {
         set1.setCircleSize(4f);
         set1.setDrawCircleHole(false);
         set1.disableDashedLine();
-        set1.setValueTextSize(9f);
         set1.setFillAlpha(65);
+        set1.setValueTextSize(0);
+        set1.setValueTextColor(Color.parseColor("#FFFFFF"));
         set1.setFillColor(Color.BLACK);
 //        set1.setDrawFilled(true);
         // set1.setShader(new LinearGradient(0, 0, 0, mChart.getHeight(),
@@ -136,5 +169,19 @@ public class OverviewFragment extends Fragment {
 
         // set data
         chart.setData(data);
+    }
+
+    private String convertDate(String date) {
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        DateFormat outputFormat = new SimpleDateFormat("dd MMM yyyy HH:mm");
+        Date parsed = null;
+        try {
+            parsed = inputFormat.parse(date);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String outputText = outputFormat.format(parsed);
+        return outputText;
     }
 }
