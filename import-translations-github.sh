3a491148363adaa21b6d933a16cf759b84b774ba
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-10-07 20:23:43 +0200

    Test new way to import translations.

--- import-translations-github.sh
+++ import-translations-github.sh
@@ -2,10 +2,6 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting translation import\n"
 
-  #copy data we're interested in to other place
-  mkdir $HOME/android/
-  cp -R app/src/main/res/* $HOME/android/
-
   #go to home and setup git
   cd $HOME
   git config --global user.email "glucosioproject@gmail.com"
@@ -15,15 +11,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=develop https://8ded5df0cdf373ca7b7662f00ef159f722601d54@github.com/Glucosio/android.git  develop > /dev/null
 
   #go into directory and copy data we're interested in to that directory
-  cd develop
-  cp -Ru $HOME/android/ app/src/main/res/
+  cd develop/app/src/main/res/
+  wget https://crowdin.com/downloads/crowdin-cli.jar
+  java -jar crowdin-cli.jar download
+  rm crowdin-cli.jar
+
+  cd $HOME/develop
 
   #add, commit and push files
   git add -f .
   git remote rm origin
   git remote add origin https://glucat:$GITHUB_API_KEY@github.com/Glucosio/android.git
   git add -f .
-  git commit -m "Done translation import for (build $TRAVIS_BUILD_NUMBER). [ci skip]"
+  git commit -m "Automatic translation import (build $TRAVIS_BUILD_NUMBER)."
   git push -fq origin develop > /dev/null
 
   echo -e "Done magic with coverage\n"
