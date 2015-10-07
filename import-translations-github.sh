054df59106819d5f6fbaf4bb95fca2747ce42efe
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-10-07 18:36:51 +0200

    Fix translations import on Travis.

---
+++ import-translations-github.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting translation import\n"
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "glucosioproject@gmail.com"
+  git config --global user.name "Glucat"
+
+  cd $TRAVIS_BUILD_DIR
+
+  #add, commit and push files
+  git add -f .
+  git remote rm origin
+  git remote add origin https://glucat:8ded5df0cdf373ca7b7662f00ef159f722601d54@github.com/Glucosio/android
+  git add -f .
+  git commit -m "Automatic translations import (build  $TRAVIS_BUILD_NUMBER)."
+  git push -fq origin develop > /dev/null
+
+  echo -e "Done \n"
+fi
