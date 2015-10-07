523abae4592ecd7be6b659d6fe85bd1ed909d134
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-10-07 19:41:16 +0200

    Test Travis automatic translation import in gh.

--- import-translations-github.sh
+++ import-translations-github.sh
@@ -2,20 +2,29 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting translation import\n"
 
+  #copy data we're interested in to other place
+  mkdir $HOME/android/
+  cp -R app/src/main/res/ $HOME/android/
+
   #go to home and setup git
   cd $HOME
   git config --global user.email "glucosioproject@gmail.com"
   git config --global user.name "Glucat"
 
-  cd $TRAVIS_BUILD_DIR
+  #clone gh-pages branch
+  git clone --quiet --branch=translation_test https://8ded5df0cdf373ca7b7662f00ef159f722601d54@github.com/Glucosio/android.git  translation_test > /dev/null
+
+  #go into directory and copy data we're interested in to that directory
+  cd translation_test
+  cp -Rf $HOME/android/ app/src/main/
 
   #add, commit and push files
   git add -f .
   git remote rm origin
-  git remote add origin https://glucat:8ded5df0cdf373ca7b7662f00ef159f722601d54@github.com/Glucosio/android
+  git remote add origin https://glucat:$GITHUB_API_KEY@github.com/Glucosio/android.git
   git add -f .
-  git commit -m "Automatic translations import (build  $TRAVIS_BUILD_NUMBER)."
-  git push -fq origin develop > /dev/null
+  git commit -m "Done translation import for (build $TRAVIS_BUILD_NUMBER)."
+  git push -fq origin translation_test > /dev/null
 
-  echo -e "Done \n"
+  echo -e "Done magic with coverage\n"
 fi
