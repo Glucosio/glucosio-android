ac7c46dbbcc95aafaefc2640aae9143bee84a1d5
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-10-07 18:52:05 +0200

    Fix translations import on Travis.

--- upload-gh-pages.sh
+++ upload-gh-pages.sh
@@ -22,7 +22,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #add, commit and push files
   git add -f .
   git remote rm origin
-  git remote add origin https://glucat:8ded5df0cdf373ca7b7662f00ef159f722601d54@github.com/Glucosio/glucosio.github.io.git
+  git remote add origin https://glucat:$GITHUB_API_KEY@github.com/Glucosio/glucosio.github.io.git
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin master > /dev/null
