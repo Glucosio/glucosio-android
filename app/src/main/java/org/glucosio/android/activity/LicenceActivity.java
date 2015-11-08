87e322b2a0999eb04e0c8787e8e345d93c27f47d
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-11-08 12:02:48 +0100

    Update licences.

--- app/src/main/java/org/glucosio/android/activity/LicenceActivity.java
+++ app/src/main/java/org/glucosio/android/activity/LicenceActivity.java
@@ -31,13 +31,13 @@ public class LicenceActivity extends AppCompatActivity {
 
 
         if (url != null && url.equals("privacy")) {
-            webView.loadUrl("http://www.glucosio.org/privacy");
+            webView.loadUrl("http://www.glucosio.org/privacy/");
             getSupportActionBar().setTitle(getResources().getString(R.string.preferences_privacy));
         } else if (url != null && url.equals("open_source")) {
-            webView.loadUrl("http://www.glucosio.org/licences");
+            webView.loadUrl("http://www.glucosio.org/third-party-licenses/");
             getSupportActionBar().setTitle(getResources().getString(R.string.preferences_licences_open));
         } else {
-            webView.loadUrl("http://www.glucosio.org/terms");
+            webView.loadUrl("http://www.glucosio.org/terms/");
             getSupportActionBar().setTitle(getResources().getString(R.string.preferences_terms));
         }
 
