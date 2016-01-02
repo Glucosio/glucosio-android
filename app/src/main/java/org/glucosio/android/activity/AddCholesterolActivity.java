61da1f3807331ee30032c1dc71940de9c1b107b9
Paolo Rotolo <paolorotolo@ubuntu.com>
2016-01-02 20:23:42 +0100

    Add logic for new metrics.

---
+++ app/src/main/java/org/glucosio/android/activity/AddCholesterolActivity.java
@@ -0,0 +1,166 @@
+package org.glucosio.android.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import org.glucosio.android.R;
+import org.glucosio.android.presenter.AddCholesterolPresenter;
+import org.glucosio.android.tools.FormatDateTime;
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+
+public class AddCholesterolActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+    private FloatingActionButton doneFAB;
+    private TextView addTimeTextView;
+    private TextView addDateTextView;
+    private TextView totalChoTextView;
+    private TextView LDLChoTextView;
+    private TextView HDLChoTextView;
+
+    AddCholesterolPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cholesterol);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setElevation(2);
+        }
+
+        presenter = new AddCholesterolPresenter(this);
+        presenter.getCurrentTime();
+
+        doneFAB = (FloatingActionButton) findViewById(R.id.done_fab);
+        addTimeTextView = (TextView) findViewById(R.id.dialog_add_time);
+        addDateTextView = (TextView) findViewById(R.id.dialog_add_date);
+        totalChoTextView = (TextView) findViewById(R.id.cholesterol_add_value_total);
+        LDLChoTextView = (TextView) findViewById(R.id.cholesterol_add_value_ldl);
+        HDLChoTextView = (TextView) findViewById(R.id.cholesterol_add_value_hdl);
+
+        FormatDateTime formatDateTime = new FormatDateTime(getApplicationContext());
+        addDateTextView.setText(presenter.getReadingDay() + "/" + presenter.getReadingMonth() + "/" + presenter.getReadingYear());
+        addTimeTextView.setText(formatDateTime.getCurrentTime());
+        addDateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        AddCholesterolActivity.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+                dpd.setMaxDate(now);
+            }
+        });
+
+        addTimeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                if (android.text.format.DateFormat.is24HourFormat(getApplicationContext())) {
+                    TimePickerDialog tpd = TimePickerDialog.newInstance(AddCholesterolActivity.this, now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), true);
+                    tpd.show(getFragmentManager(), "Timepickerdialog");
+                } else {
+                    TimePickerDialog tpd = TimePickerDialog.newInstance(AddCholesterolActivity.this, now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), false);
+                    tpd.show(getFragmentManager(), "Timepickerdialog");
+                }
+            }
+        });
+        doneFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogOnAddButtonPressed();
+            }
+        });
+    }
+
+    private void dialogOnAddButtonPressed() {
+            presenter.dialogOnAddButtonPressed(addTimeTextView.getText().toString(),
+                    addDateTextView.getText().toString(), totalChoTextView.getText().toString(), LDLChoTextView.getText().toString(), HDLChoTextView.getText().toString());
+    }
+
+    public void showErrorMessage() {
+        Toast.makeText(getApplicationContext(), getString(R.string.dialog_error2), Toast.LENGTH_SHORT).show();
+    }
+
+    public void finishActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        TextView addTime = (TextView) findViewById(R.id.dialog_add_time);
+        DecimalFormat df = new DecimalFormat("00");
+
+        presenter.setReadingHour(df.format(hourOfDay));
+        presenter.setReadingMinute(df.format(minute));
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        cal.set(Calendar.MINUTE, minute);
+        FormatDateTime formatDateTime = new FormatDateTime(getApplicationContext());
+        addTime.setText(formatDateTime.getTime(cal));
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        TextView addDate = (TextView) findViewById(R.id.dialog_add_date);
+        DecimalFormat df = new DecimalFormat("00");
+
+        presenter.setReadingYear(year + "");
+        presenter.setReadingMonth(df.format(monthOfYear + 1));
+        presenter.setReadingDay(df.format(dayOfMonth));
+
+        String date = +dayOfMonth + "/" + presenter.getReadingMonth() + "/" + presenter.getReadingYear();
+        addDate.setText(date);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (Integer.parseInt(android.os.Build.VERSION.SDK) > 5
+                && keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishActivity();
+    }
+}
---
+++ app/src/main/java/org/glucosio/android/presenter/AddHB1ACPresenter.java
@@ -0,0 +1,129 @@
+package org.glucosio.android.presenter;
+
+import org.glucosio.android.activity.AddHB1ACActivity;
+import org.glucosio.android.db.DatabaseHandler;
+import org.glucosio.android.db.HB1ACReading;
+import org.glucosio.android.tools.ReadingTools;
+import org.glucosio.android.tools.SplitDateTime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AddHB1ACPresenter {
+    private DatabaseHandler dB;
+    private AddHB1ACActivity activity;
+
+    private ReadingTools rTools;
+    private String readingYear;
+    private String readingMonth;
+    private String readingDay;
+    private String readingHour;
+    private String readingMinute;
+
+
+    public AddHB1ACPresenter(AddHB1ACActivity addHB1ACActivity) {
+        this.activity= addHB1ACActivity;
+        dB = new DatabaseHandler(addHB1ACActivity.getApplicationContext());
+    }
+
+
+    public void getCurrentTime(){
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date formatted = Calendar.getInstance().getTime();
+
+        SplitDateTime addSplitDateTime = new SplitDateTime(formatted, inputFormat);
+
+        this.readingYear = addSplitDateTime.getYear();
+        this.readingMonth = addSplitDateTime.getMonth();
+        this.readingDay = addSplitDateTime.getDay();
+        this.readingHour = addSplitDateTime.getHour();
+        this.readingMinute = addSplitDateTime.getMinute();
+    }
+
+    public int timeToSpinnerType() {
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date formatted = Calendar.getInstance().getTime();
+
+        SplitDateTime addSplitDateTime = new SplitDateTime(formatted, inputFormat);
+        int hour = Integer.parseInt(addSplitDateTime.getHour());
+
+        return hourToSpinnerType(hour);
+    }
+
+    public int hourToSpinnerType(int hour){
+        rTools = new ReadingTools();
+        return rTools.hourToSpinnerType(hour);
+    }
+
+    public void dialogOnAddButtonPressed(String time, String date, String reading){
+        if (validateDate(date) && validateTime(time)) {
+
+            Calendar cal = Calendar.getInstance();
+            cal.set(Integer.parseInt(readingYear), Integer.parseInt(readingMonth)-1, Integer.parseInt(readingDay), Integer.parseInt(readingHour), Integer.parseInt(readingMinute));
+            Date finalDateTime = cal.getTime();
+            int finalReading = Integer.parseInt(reading);
+            HB1ACReading hReading = new HB1ACReading(finalReading, finalDateTime);
+
+            dB.addHB1ACReading(hReading);
+            activity.finishActivity();
+        } else {
+            activity.showErrorMessage();
+        }
+    }
+
+    private boolean validateTime(String time){
+        return !time.equals("");
+    }
+    private boolean validateDate(String date){
+        return !date.equals("");
+    }
+
+    // Getters and Setters
+
+    public String getUnitMeasuerement(){
+        return dB.getUser(1).getPreferred_unit();
+    }
+
+    public String getReadingYear() {
+        return readingYear;
+    }
+
+    public String getReadingMonth() {
+        return readingMonth;
+    }
+
+    public String getReadingDay() {
+        return readingDay;
+    }
+
+    public String getReadingHour() {
+        return readingHour;
+    }
+
+    public String getReadingMinute() {
+        return readingMinute;
+    }
+
+    public void setReadingYear(String readingYear) {
+        this.readingYear = readingYear;
+    }
+
+    public void setReadingMonth(String readingMonth) {
+        this.readingMonth = readingMonth;
+    }
+
+    public void setReadingDay(String readingDay) {
+        this.readingDay = readingDay;
+    }
+
+    public void setReadingHour(String readingHour) {
+        this.readingHour = readingHour;
+    }
+
+    public void setReadingMinute(String readingMinute) {
+        this.readingMinute = readingMinute;
+    }
+
+}
