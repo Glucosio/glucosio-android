61ad16ebfb251e96ea5d0f96ce07441800f35c28
Paolo Rotolo <paolorotolo@ubuntu.com>
2016-04-20 23:29:07 +0200

    Add method to restore from drive backup.

--- app/src/main/java/org/glucosio/android/activity/BackupActivity.java
+++ app/src/main/java/org/glucosio/android/activity/BackupActivity.java
@@ -20,6 +20,9 @@
 
 package org.glucosio.android.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -35,8 +38,13 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi;
 import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
 
 import org.glucosio.android.GlucosioApplication;
 import org.glucosio.android.R;
@@ -49,6 +57,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -57,17 +66,21 @@ import io.realm.Realm;
 
 public class BackupActivity extends AppCompatActivity {
 
-    Backup backup;
-    GoogleApiClient mGoogleApiClient;
-    String TAG = "glucosio_drive_backup";
-    Button backupButton;
+    private Backup backup;
+    private GoogleApiClient mGoogleApiClient;
+    private String TAG = "glucosio_drive_backup";
+    private Button backupButton;
+    private Button restoreButton;
+    private DriveFile restoredFile;
+    private Realm realm;
+
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.backup_drive_activity);
 
-        final Realm realm = new DatabaseHandler(getApplicationContext()).getRealmIstance();
+        realm = new DatabaseHandler(getApplicationContext()).getRealmIstance();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getResources().getString(R.string.title_activity_backup));
@@ -78,10 +91,19 @@ public class BackupActivity extends AppCompatActivity {
         mGoogleApiClient = backup.getClient();
 
         backupButton = (Button) findViewById(R.id.activity_backup_drive_button_backup);
+        restoreButton = (Button) findViewById(R.id.activity_backup_drive_button_restore);
+
         backupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 uploadToDrive(new File(realm.getPath()));
+            }
+        });
+
+        restoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restoreFromDrive();
             }
         });
     }
@@ -140,6 +162,95 @@ public class BackupActivity extends AppCompatActivity {
                 });
     }
 
+    private void restoreFromDrive(){
+        // First we search for plain text file named glucosio.realm
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.TITLE, "glucosio.realm"))
+                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "text/plain"))
+                .build();
+
+        Drive.DriveApi.query(mGoogleApiClient, query)
+                .setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+
+                    @Override
+                    public void onResult(DriveApi.MetadataBufferResult result) {
+                        if (!result.getStatus().isSuccess()) {
+                            Log.e(TAG, "Problem while retrieving results");
+                            return;
+                        }
+                        Log.e(TAG,result.toString());
+                        Log.e(TAG, result.getMetadataBuffer().getCount()+"");
+                        // Pick the first file in App Folder
+                        if (result.getMetadataBuffer().get(0).isInAppFolder()){
+                            restoredFile = result.getMetadataBuffer().get(0).getDriveId().asDriveFile();
+                            result.release();
+                        }
+
+                        if (restoredFile != null) {
+                            // Now get the file from the id
+                            restoredFile.open(mGoogleApiClient, DriveFile.MODE_READ_ONLY, null)
+                                    .setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
+                                        @Override
+                                        public void onResult(DriveApi.DriveContentsResult result) {
+                                            if (!result.getStatus().isSuccess()) {
+                                                showErrorDialog();
+                                                return;
+                                            }
+
+                                            // DriveContents object contains pointers
+                                            // to the actual byte stream
+                                            DriveContents contents = result.getDriveContents();
+                                            InputStream input = contents.getInputStream();
+
+                                            try {
+                                                File file = new File(realm.getPath(), "default.realm");
+                                                OutputStream output = new FileOutputStream(file);
+                                                try {
+                                                    try {
+                                                        byte[] buffer = new byte[4 * 1024]; // or other buffer size
+                                                        int read;
+
+                                                        while ((read = input.read(buffer)) != -1) {
+                                                            output.write(buffer, 0, read);
+                                                        }
+                                                        output.flush();
+                                                    } finally {
+                                                        output.close();
+                                                    }
+                                                } catch (Exception e) {
+                                                    e.printStackTrace();
+                                                }
+                                            } catch (FileNotFoundException e) {
+                                                e.printStackTrace();
+                                            } finally {
+                                                try {
+                                                    input.close();
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                        }
+                                    });
+
+                            // Reboot app
+                            Intent mStartActivity = new Intent(getApplicationContext(), MainActivity.class);
+                            int mPendingIntentId = 123456;
+                            PendingIntent mPendingIntent = PendingIntent.getActivity(getApplicationContext(), mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+                            AlarmManager mgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+                            mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                            System.exit(0);
+                        } else {
+                            showErrorDialogRestore();
+                        }
+                    }
+                });
+
+    }
+
+    private void showErrorDialogRestore() {
+        Toast.makeText(getApplicationContext(), R.string.activity_backup_drive_error_restore,Toast.LENGTH_SHORT).show();
+    }
+
     final private ResultCallback<DriveFolder.DriveFileResult> fileCallback = new
             ResultCallback<DriveFolder.DriveFileResult>() {
                 @Override
@@ -154,6 +265,7 @@ public class BackupActivity extends AppCompatActivity {
                     finish();
                 }
             };
+
 
     private void showSuccessDialog() {
         Toast.makeText(getApplicationContext(), R.string.activity_backup_drive_success, Toast.LENGTH_SHORT).show();
--- app/src/main/java/org/glucosio/android/backup/GoogleDriveBackup.java
+++ app/src/main/java/org/glucosio/android/backup/GoogleDriveBackup.java
@@ -3,8 +3,6 @@ package org.glucosio.android.backup;
 import android.app.Activity;
 import android.content.IntentSender;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
