f759994f3b369fedec07e0214c8eab763c207b56
Eugen Martynov <eugenemartynov@gmail.com>
2016-05-26 22:49:14 +0200

    Added more tests

--- app/src/test/java/org/glucosio/android/activity/A1cCalculatorActivityTest.java
+++ app/src/test/java/org/glucosio/android/activity/A1cCalculatorActivityTest.java
@@ -1,5 +1,6 @@
 package org.glucosio.android.activity;
 
+import org.glucosio.android.R;
 import org.glucosio.android.RobolectricTest;
 import org.glucosio.android.TestGlucosioApplication;
 import org.glucosio.android.presenter.A1CCalculatorPresenter;
@@ -8,7 +9,9 @@ import org.junit.Test;
 import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
 
+import static org.assertj.android.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class A1cCalculatorActivityTest extends RobolectricTest {
@@ -29,5 +32,21 @@ public class A1cCalculatorActivityTest extends RobolectricTest {
     private A1CCalculatorPresenter getA1cCalculatorPresenter() {
         //noinspection ConstantConditions
         return ((TestGlucosioApplication) RuntimeEnvironment.application).createA1cCalculatorPresenter(null);
+    }
+
+    @Test
+    public void ShouldBindViews_WhenCreated() throws Exception {
+        assertThat(activity.A1cUnitTextView).isNotNull();
+        assertThat(activity.A1CTextView).isNotNull();
+        assertThat(activity.glucoseUnit).isNotNull();
+    }
+
+    @Test
+    public void ShouldSetMmolToMolAsUnit_WhenUserSettingsIsPercentage() throws Exception {
+        when(getA1cCalculatorPresenter().getA1cUnit()).thenReturn("not percentage");
+
+        activity = Robolectric.buildActivity(A1cCalculatorActivity.class).create().get();
+
+        assertThat(activity.A1cUnitTextView).hasText(R.string.mmol_mol);
     }
 }
