4f00d27a2cd194bdff387d4777b1841bad24502e
Eugen Martynov <eugenemartynov@gmail.com>
2016-07-11 07:06:10 +0200

    Extracted method and added first test for LocaleHelper

--- app/src/main/java/org/glucosio/android/tools/LocaleHelper.java
+++ app/src/main/java/org/glucosio/android/tools/LocaleHelper.java
@@ -6,10 +6,18 @@ import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 
+import org.glucosio.android.BuildConfig;
+import org.glucosio.android.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class LocaleHelper {
-    public Locale getLocale(String languageTag) {
+    private Locale getLocale(String languageTag) {
         String[] values = languageTag.split("-");
         switch (values.length) {
             case 3:
@@ -40,4 +48,41 @@ public class LocaleHelper {
         Locale.setDefault(locale);
     }
 
+    @NonNull
+    public List<String> getLocalesWithTranslation(final Resources resources) {
+        String[] languages = BuildConfig.TRANSLATION_ARRAY;
+        Set<String> availableLanguagesSet = new HashSet<>();
+        // We always support english
+        availableLanguagesSet.add("en");
+
+        // Get english string to confront
+        // I know, it's a weird workaround
+        // Sorry :/
+        String englishString = "Automatic backup";
+
+        for (String localString : languages) {
+            // For each locale, check if we have translations
+            Configuration conf = resources.getConfiguration();
+            Locale savedLocale = conf.locale;
+            conf.locale = getLocale(localString);
+            resources.updateConfiguration(conf, null);
+
+            // Retrieve an example string from this locale
+            String localizedString = resources.getString(R.string.activity_backup_drive_automatic);
+
+            if (!englishString.equals(localizedString)) {
+                // if english string is not the same of localized one
+                // a translation is available
+                availableLanguagesSet.add(localString);
+            }
+
+            // restore original locale
+            conf.locale = savedLocale;
+            resources.updateConfiguration(conf, null);
+        }
+
+        List<String> availableLanguagesList = new ArrayList<>(availableLanguagesSet);
+        Collections.sort(availableLanguagesList);
+        return availableLanguagesList;
+    }
 }
