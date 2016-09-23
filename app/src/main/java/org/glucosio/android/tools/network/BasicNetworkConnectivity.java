8c95fa13ec78721571f15919c7ea30176922f87f
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-09-23 22:28:47 +0200

    Create and improved the basic network availability implementation

--- app/src/main/java/org/glucosio/android/tools/network/BasicNetworkConnectivity.java
+++ app/src/main/java/org/glucosio/android/tools/network/BasicNetworkConnectivity.java
@@ -23,11 +23,18 @@ package org.glucosio.android.tools.network;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import java.lang.ref.WeakReference;
 
 public class BasicNetworkConnectivity implements NetworkConnectivity {
 
-  public boolean isConnected(Context context) {
-    ConnectivityManager cm = provideConnectivityService(context);
+  private WeakReference<Context> context;
+
+  public BasicNetworkConnectivity(Context context) {
+    this.context = new WeakReference<>(context);
+  }
+
+  public boolean isConnected() {
+    ConnectivityManager cm = provideConnectivityService(context.get());
     NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
     return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
   }
--- app/src/main/java/org/glucosio/android/tools/network/NetworkConnectivity.java
+++ app/src/main/java/org/glucosio/android/tools/network/NetworkConnectivity.java
@@ -1,7 +1,5 @@
 package org.glucosio.android.tools.network;
 
-/**
- * Created by alexsimo on 9/23/16.
- */
 public interface NetworkConnectivity {
+  boolean isConnected();
 }
