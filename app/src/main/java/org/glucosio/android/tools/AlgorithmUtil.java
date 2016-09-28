7b7682d06c122dc5d11d3e8c202d799c48e802c1
Markus Kaindl <kaindl@users.noreply.github.com>
2016-09-29 01:25:22 +0200

    FreestyleLibre: divide raw value by 8.5
    xdrip+/LimiTTer are dividing by 8.5, seems to work good for many people
    there, including me

--- app/src/main/java/org/glucosio/android/tools/AlgorithmUtil.java
+++ app/src/main/java/org/glucosio/android/tools/AlgorithmUtil.java
@@ -44,7 +44,7 @@ public class AlgorithmUtil {
     }
 
     private static int getGlucose(byte[] bytes) {
-        return ((256 * (bytes[0] & 0xFF) + (bytes[1] & 0xFF)) & 0x3FFF) / 10;
+        return (int)(((256 * (bytes[0] & 0xFF) + (bytes[1] & 0xFF)) & 0x0FFF) / 8.5);
     }
 
     public interface AlertRule {
