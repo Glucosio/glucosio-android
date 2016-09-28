53ac78f3cf42b0d4a0ff40c7b4c66d89c32c79a0
Markus Kaindl <kaindl@users.noreply.github.com>
2016-09-29 01:29:32 +0200

    FreesyleLibre: use latest value
    We wanted to use the latest history value, but used the oldest one,
    because array is filled from newest to oldest
    Also we should use trend values instead of history values, as the
    newest history value can be up to 15 minutes old

--- app/src/main/java/org/glucosio/android/activity/FreestyleLibreActivity.java
+++ app/src/main/java/org/glucosio/android/activity/FreestyleLibreActivity.java
@@ -176,7 +176,7 @@ public class FreestyleLibreActivity extends Activity {
 
         // Apply values in TextViews
         // TODO: Add check for mmol/L
-        readingTextView.setText(mResult.history.get(mResult.history.size()-1).glucose(false));
+        readingTextView.setText(mResult.trend.get(0).glucose(false));
 
         new Runnable() {
             @Override
@@ -196,7 +196,7 @@ public class FreestyleLibreActivity extends Activity {
             // Start AddGlucose Activity passing the reading value
             Intent intent = new Intent(getApplicationContext(), AddGlucoseActivity.class);
             Bundle bundle = new Bundle();
-            String currentGlucose = mResult.history.get(mResult.history.size()-1).glucose(false);
+            String currentGlucose = mResult.trend.get(0).glucose(false);
             bundle.putString("reading", currentGlucose + "");
             intent.putExtras(bundle);
             startActivity(intent);
