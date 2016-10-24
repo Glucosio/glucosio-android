672d33488fa3b5f69494f31635edbf2dadbcbc45
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2016-10-24 12:40:27 +0900

    Remove Auto-generated TODO stub

--- app/src/main/java/org/glucosio/android/tools/NotDismissableEditText.java
+++ app/src/main/java/org/glucosio/android/tools/NotDismissableEditText.java
@@ -30,12 +30,10 @@ public class NotDismissableEditText extends EditText {
 
     public NotDismissableEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public boolean onKeyPreIme(int keyCode, KeyEvent event) {
-        // TODO Auto-generated method stub
         return true;
         //return super.onKeyPreIme(keyCode, event);
     }
