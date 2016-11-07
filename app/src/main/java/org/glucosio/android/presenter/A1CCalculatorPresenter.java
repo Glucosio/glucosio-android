e47dfa01d41e76e70a60bc1ab60d62cc18d4038c
raacker <haven.cpp@gmail.com>
2016-11-07 14:56:42 +0900

    Add tests to A1CCalculatorPresenterTest - Swap code to give more readability   : using ! at start of boolean equation is not good for readability
    - Add test cases for other unit preference

--- app/src/main/java/org/glucosio/android/presenter/A1CCalculatorPresenter.java
+++ app/src/main/java/org/glucosio/android/presenter/A1CCalculatorPresenter.java
@@ -54,10 +54,10 @@ public class A1CCalculatorPresenter {
         } else {
             convertedA1C = converter.glucoseToA1C(converter.glucoseToMgDl(Double.parseDouble(glucose)));
         }
-        if (!"percentage".equals(user.getPreferred_unit_a1c())) {
-            return converter.a1cNgspToIfcc(convertedA1C);
-        } else {
+        if ("percentage".equals(user.getPreferred_unit_a1c())) {
             return convertedA1C;
+        } else {
+            return converter.a1cNgspToIfcc(convertedA1C);
         }
     }
 
