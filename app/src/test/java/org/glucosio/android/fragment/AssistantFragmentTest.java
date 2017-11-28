3bf51e52cac1d0e7e99f1ee234cc57f5960860ae
Zemlyakov Stas <zemlyakov_sv@magnit.ru>
2017-11-28 08:22:35 +0300

    Updated robolectric and robolectric.shadows-support-v4 versions. Now they are different but no dependency conflict. MainActivityTest import fixed.

--- app/src/test/java/org/glucosio/android/fragment/AssistantFragmentTest.java
+++ app/src/test/java/org/glucosio/android/fragment/AssistantFragmentTest.java
@@ -3,10 +3,13 @@ package org.glucosio.android.fragment;
 import org.glucosio.android.RobolectricTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(RobolectricTestRunner.class)
 public class AssistantFragmentTest extends RobolectricTest {
     private AssistantFragment fragment;
 
