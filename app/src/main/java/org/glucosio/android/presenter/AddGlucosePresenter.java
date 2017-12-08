143b57e6b33e5b72ac62f22fa0f5dda20158e820
Zemlyakov Stas <zemlyakov_sv@magnit.ru>
2017-12-08 10:52:04 +0300

    Test case with mg/Dl added.

--- app/src/main/java/org/glucosio/android/presenter/AddGlucosePresenter.java
+++ app/src/main/java/org/glucosio/android/presenter/AddGlucosePresenter.java
@@ -122,7 +122,7 @@ public class AddGlucosePresenter extends AddReadingPresenter {
 
         boolean isReadingAdded;
         int readingValue;
-        if ("mg/dL".equals(getUnitMeasuerement())) {
+        if ("mg/dL".equals(getUnitMeasuerement())) { //HERE
             readingValue = number.intValue();
         } else {
             readingValue = GlucosioConverter.glucoseToMgDl(number.doubleValue());
