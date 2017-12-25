932e9b0985f61d9c1fb89db7f5436bf36eda2252
Haven Kim <haven.cpp@gmail.com>
2017-12-24 22:15:14 -0800

    Add fully testcase of GlucosioConverter (#338)
    It doesn't harm so I decided to merge it

---
+++ app/src/test/java/org/glucosio/android/tools/GlucosioConverterTest.java
@@ -0,0 +1,57 @@
+package org.glucosio.android.tools;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GlucosioConverterTest {
+    @Test
+    public void ShouldRoundToFloor_WhenLowerValueOfExactPointIsGiven(){
+        assertThat(GlucosioConverter.round(10.123, 2)).isEqualTo(10.12);
+    }
+
+    @Test
+    public void ShouldRoundToCeil_WhenHigherValueOfExactPointIsGiven(){
+        assertThat(GlucosioConverter.round(10.126, 2)).isEqualTo(10.13);
+    }
+
+    @Test
+    public void ShouldConvertMmolToMgDlExactly_WhenMgDlNeeded(){
+        assertThat(GlucosioConverter.glucoseToMgDl(1)).isEqualTo(18);
+    }
+
+    @Test
+    public void ShouldConvertMgDlToMmolExactly_WhenMmolNeeded(){
+        assertThat(GlucosioConverter.glucoseToMmolL(18)).isEqualTo(1);
+    }
+
+    @Test
+    public void ShouldConvertMgDlToA1cExactly_WhenA1CNeeded(){
+        assertThat(GlucosioConverter.glucoseToA1C(18)).isEqualTo(2.25);
+    }
+
+    @Test
+    public void ShouldConvertA1CToGlucoseExactly_WhenGlucoseNeeded(){
+        assertThat(GlucosioConverter.a1cToGlucose(2.25)).isEqualTo(17.88);
+    }
+
+    @Test
+    public void ShouldConvertKgToLbExactly_WhenLbNeeded(){
+        assertThat(GlucosioConverter.kgToLb(60)).isEqualTo(132);
+    }
+
+    @Test
+    public void ShouldConvertlbToKgExactly_WhenKgNeeded(){
+        assertThat(GlucosioConverter.lbToKg(132)).isEqualTo(59);
+    }
+
+    @Test
+    public void ShouldConvertPercentageToA1cExactly_WhenPercentageIsUsed(){
+        assertThat(GlucosioConverter.a1cNgspToIfcc(5)).isEqualTo(31.13);
+    }
+
+    @Test
+    public void ShouldConvertA1cToPercentageExactly_WhenPercentageNeeded(){
+        assertThat(GlucosioConverter.a1cIfccToNgsp(31.13)).isEqualTo(5);
+    }
+}
