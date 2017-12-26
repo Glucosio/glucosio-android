411a6d2e4f119d7020ccc659e273601d1cf98fd3
Eugen Martynov <eugenemartynov@gmail.com>
2017-12-26 10:07:20 +0100

    Fixed and formatted tests after merge

--- app/src/test/java/org/glucosio/android/tools/GlucosioConverterTest.java
+++ app/src/test/java/org/glucosio/android/tools/GlucosioConverterTest.java
@@ -3,55 +3,56 @@ package org.glucosio.android.tools;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class GlucosioConverterTest {
     @Test
-    public void ShouldRoundToFloor_WhenLowerValueOfExactPointIsGiven(){
+    public void ShouldRoundToFloor_WhenLowerValueOfExactPointIsGiven() {
         assertThat(GlucosioConverter.round(10.123, 2)).isEqualTo(10.12);
     }
 
     @Test
-    public void ShouldRoundToCeil_WhenHigherValueOfExactPointIsGiven(){
+    public void ShouldRoundToCeil_WhenHigherValueOfExactPointIsGiven() {
         assertThat(GlucosioConverter.round(10.126, 2)).isEqualTo(10.13);
     }
 
     @Test
-    public void ShouldConvertMmolToMgDlExactly_WhenMgDlNeeded(){
+    public void ShouldConvertMmolToMgDlExactly_WhenMgDlNeeded() {
         assertThat(GlucosioConverter.glucoseToMgDl(1)).isEqualTo(18);
     }
 
     @Test
-    public void ShouldConvertMgDlToMmolExactly_WhenMmolNeeded(){
+    public void ShouldConvertMgDlToMmolExactly_WhenMmolNeeded() {
         assertThat(GlucosioConverter.glucoseToMmolL(18)).isEqualTo(1);
     }
 
     @Test
-    public void ShouldConvertMgDlToA1cExactly_WhenA1CNeeded(){
+    public void ShouldConvertMgDlToA1cExactly_WhenA1CNeeded() {
         assertThat(GlucosioConverter.glucoseToA1C(18)).isEqualTo(2.25);
     }
 
     @Test
-    public void ShouldConvertA1CToGlucoseExactly_WhenGlucoseNeeded(){
+    public void ShouldConvertA1CToGlucoseExactly_WhenGlucoseNeeded() {
         assertThat(GlucosioConverter.a1cToGlucose(2.25)).isEqualTo(17.88);
     }
 
     @Test
-    public void ShouldConvertKgToLbExactly_WhenLbNeeded(){
-        assertThat(GlucosioConverter.kgToLb(60)).isEqualTo(132);
+    public void ShouldConvertKgToLbExactly_WhenLbNeeded() {
+        assertThat(GlucosioConverter.kgToLb(60)).isEqualTo(132.27, offset(0.01));
     }
 
     @Test
-    public void ShouldConvertlbToKgExactly_WhenKgNeeded(){
-        assertThat(GlucosioConverter.lbToKg(132)).isEqualTo(59);
+    public void ShouldConvertLbToKgExactly_WhenKgNeeded() {
+        assertThat(GlucosioConverter.lbToKg(132)).isEqualTo(59.87, offset(0.01));
     }
 
     @Test
-    public void ShouldConvertPercentageToA1cExactly_WhenPercentageIsUsed(){
+    public void ShouldConvertPercentageToA1cExactly_WhenPercentageIsUsed() {
         assertThat(GlucosioConverter.a1cNgspToIfcc(5)).isEqualTo(31.13);
     }
 
     @Test
-    public void ShouldConvertA1cToPercentageExactly_WhenPercentageNeeded(){
+    public void ShouldConvertA1cToPercentageExactly_WhenPercentageNeeded() {
         assertThat(GlucosioConverter.a1cIfccToNgsp(31.13)).isEqualTo(5);
     }
 }
