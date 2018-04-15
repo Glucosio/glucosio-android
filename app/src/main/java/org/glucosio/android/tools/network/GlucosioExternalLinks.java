b2a670292fbb326d37421e11523af8e9fae286b9
Chris Walters <walters@id.com>
2018-04-15 21:30:44 +0100

    Modify external links to use https

--- app/src/main/java/org/glucosio/android/tools/network/GlucosioExternalLinks.java
+++ app/src/main/java/org/glucosio/android/tools/network/GlucosioExternalLinks.java
@@ -1,8 +1,8 @@
 package org.glucosio.android.tools.network;
 
 public class GlucosioExternalLinks {
-  public static final String TERMS = "http://www.glucosio.org/terms/";
-  public static final String LICENSES = "http://www.glucosio.org/third-party-licenses/";
-  public static final String PRIVACY = "http://www.glucosio.org/privacy/";
-  public static final String THANKS = "http://www.glucosio.org/2015/10/12/thanks-for-contributors/";
+  public static final String TERMS = "https://www.glucosio.org/terms/";
+  public static final String LICENSES = "https://www.glucosio.org/third-party-licenses/";
+  public static final String PRIVACY = "https://www.glucosio.org/privacy/";
+  public static final String THANKS = "https://www.glucosio.org/2015/10/12/thanks-for-contributors/";
 }
